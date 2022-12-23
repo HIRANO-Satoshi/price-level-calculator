6a2532aa77707754d5d6139c57df1816a18fe643
HIRANO Satoshi <happy.hirano@gmail.com>
2022-12-23 18:46:13 +0900

    done yarn start

--- app/aurelia_project.old/generators/generator.ts
+++ app/aurelia_project.old/generators/generator.ts
@@ -5,34 +5,29 @@ import {Project, ProjectItem, CLIOptions, UI} from 'aurelia-cli';
 export default class GeneratorGenerator {
   constructor(private project: Project, private options: CLIOptions, private ui: UI) { }
 
-  async execute() {
-    const name = await this.ui.ensureAnswer(
-      this.options.args[0],
-      'What would you like to call the generator?'
-    );
+  execute() {
+    return this.ui
+      .ensureAnswer(this.options.args[0], 'What would you like to call the generator?')
+      .then(name => {
+        let fileName = this.project.makeFileName(name);
+        let className = this.project.makeClassName(name);
 
-    let fileName = this.project.makeFileName(name);
-    let className = this.project.makeClassName(name);
+        this.project.generators.add(
+          ProjectItem.text(`${fileName}.ts`, this.generateSource(className))
+        );
 
-    this.project.generators.add(
-      ProjectItem.text(`${fileName}.ts`, this.generateSource(className))
-    );
-
-    await this.project.commitChanges()
-    await this.ui.log(`Created ${fileName}.`);
+        return this.project.commitChanges()
+          .then(() => this.ui.log(`Created ${fileName}.`));
+      });
   }
 
   generateSource(className) {
-    return `import {inject} from 'aurelia-dependency-injection';
+return `import {autoinject} from 'aurelia-dependency-injection';
 import {Project, ProjectItem, CLIOptions, UI} from 'aurelia-cli';
 
-@inject(Project, CLIOptions, UI)
+@autoinject()
 export default class ${className}Generator {
-  constructor(project, options, ui) {
-    this.project = project;
-    this.options = options;
-    this.ui = ui;
-  }
+  constructor(private project: Project, private options: CLIOptions, private ui: UI) { }
 
   execute() {
     return this.ui
@@ -57,12 +52,14 @@ export class \${className} {
   @bindable value;
 
   valueChanged(newValue, oldValue) {
-    //
+
   }
 }
+
 \`
   }
 }
-`;
+
+`
   }
 }
